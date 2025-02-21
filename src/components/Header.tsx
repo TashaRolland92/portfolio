a93a613233afa4ca71b89f003f8162e96d4279d2
TashaRolland92 <tasharolland@hotmail.co.uk>
2025-02-21 12:48:03 +0000

    reworked header component | improved alt tag

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import Nav from './Nav';
+import Logo from '../assets/svgs/diamond.svg';
 
 const Header = () => {
 	return (
@@ -6,7 +7,13 @@ const Header = () => {
 			<div className="container">
 				<div className="row">
 					<div className="col-12">
-						<Nav />
+						<div className="h-100 d-flex justify-content-between align-items-center">
+							<h1 className="default__gradient--text logo">
+								<img src={Logo} alt="Mimi Pink to Periwinkle, Gradient Diamond Icon" className="diamond-icon" />
+								Tasha
+							</h1>
+							<Nav />
+						</div>
 					</div>
 				</div>
 			</div>
