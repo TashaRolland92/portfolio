26e5c57812f8f34cff2c8ffceea7e2d1d7ea3778
TashaRolland92 <tasharolland@hotmail.co.uk>
2025-02-28 17:40:48 +0000

    code improvements

--- src/components/ProfessionalExperience.tsx
+++ src/components/ProfessionalExperience.tsx
@@ -7,6 +7,79 @@ import PP from "../assets/professional/pod-point-icon.png";
 import CD from "../assets/professional/copia-logo.png";
 import TRA_CV from "/TRA_CV.pdf";
 
+const experienceData = [
+	{
+		titleClasses: 'vertical-timeline-element-title jordy-blue',
+		title: "Frontend Engineer",
+		subtitle: "Bordeaux Index",
+		techList: [
+			'js',
+			'sass',
+			'node',
+			'php',
+		],
+		icon: BI,
+		bgColor: '#FFD6FF',
+		arrowColor: '#FFD6FF',
+	},
+	{
+		titleClasses: 'vertical-timeline-element-title jordy-blue',
+		title: "Frontend Developer",
+		subtitle: "Ernst & Young LLP",
+		description: "AngularJS, JavaScript, Trello, MS Sharepoint, HTML5, CSS3, CRM, InVision, Agile.",
+		techList: [
+			'js',
+			'css',
+			'angular',
+			'html',
+		],
+		icon: EY,
+		bgColor: '#E7C6FF',
+		arrowColor: '#E7C6FF',
+	},
+	{
+		titleClasses: 'vertical-timeline-element-title mimi-pink',
+		title: "Frontend Developer",
+		subtitle: "New Look",
+		techList: [
+			'js',
+			'css',
+			'html',
+		],
+		icon: NL,
+		bgColor: '#C8B6FF',
+		arrowColor: '#C8B6FF',
+	},
+	{
+		titleClasses: 'vertical-timeline-element-title mimi-pink',
+		title: "Frontend Developer",
+		subtitle: "Pod Point",
+		techList: [
+			'js',
+			'react',
+			'css',
+			'php',
+		],
+		icon: PP,
+		bgColor: '#B8C0FF',
+		arrowColor: '#B8C0FF',
+	},
+	{
+		titleClasses: 'vertical-timeline-element-title mimi-pink',
+		title: "Web Designer/Developer",
+		subtitle: "Copia Digital",
+		description: "HTML5, CSS3, PHP, CMS, SVN, Adobe Photoshop, D3, Balsamiq, phpMyAdmin.",
+		techList: [
+			'html',
+			'sass',
+			'php',
+		],
+		icon: CD,
+		bgColor: '#94B2F5',
+		arrowColor: '#94B2F5',
+	},
+];
+
 const ProfessionalExperience = () => {
   return (
     <section id="section3" className="professional-experience">
@@ -18,70 +91,24 @@ const ProfessionalExperience = () => {
 				</div>
 
 				<VerticalTimeline>
-					<VerticalTimelineElement
-						className="vertical-timeline-element--work vertical-timeline-element--one"
-						contentStyle={{ background: '#FFD6FF', color: '#4E4A52', fontSize: '12px'}}
-						contentArrowStyle={{ borderRight: '14px solid #FFD6FF' }}
-						date="Sept 2020 - Present"
-						iconStyle={{ background: '#FFD6FF', color: '#FFF' }}
-						icon={<img src={BI} alt="Bordeaux Index" loading="lazy" />}
-					>
-						<h3 className="vertical-timeline-element-title jordy-blue">Frontend Engineer</h3>
-						<h4 className="vertical-timeline-element-subtitle">Bordeaux Index</h4>
-						<p>JavaScript, PHP, Laravel, UI/UX, CSS3, MySql, Adobe Photoshop, Node.js, Figma, Jira, Azure, TDD, InVision.</p>
-					</VerticalTimelineElement>
-
-					<VerticalTimelineElement
-						className="vertical-timeline-element--work vertical-timeline-element--two"
-						date="Jan 2019 - Sept 2020"
-						contentStyle={{ background: '#E7C6FF', color: '#4E4A52', fontSize: '12px' }}
-						contentArrowStyle={{ borderRight: '14px solid #E7C6FF' }}
-						iconStyle={{ background: '#E7C6FF', color: '#4E4A52' }}
-						icon={<img src={EY} alt="Ernst & Young LLP" loading="lazy" />}
-					>
-						<h3 className="vertical-timeline-element-title jordy-blue">Frontend Developer</h3>
-						<h4 className="vertical-timeline-element-subtitle">Ernst & Young LLP</h4>
-						<p>AngularJS, JavaScript, Trello, MS Sharepoint, HTML5, CSS3, CRM, InVision, Agile.</p>
-					</VerticalTimelineElement>
-
-					<VerticalTimelineElement
-						className="vertical-timeline-element--work vertical-timeline-element--three"
-						date="June 2019 - Dec 2019"
-						contentStyle={{ background: '#C8B6FF', color: '#4E4A52', fontSize: '12px' }}
-						contentArrowStyle={{ borderRight: '14px solid #C8B6FF' }}
-						iconStyle={{ background: '#C8B6FF', color: '#4E4A52' }}
-						icon={<img src={NL} alt="New Look" loading="lazy" />}
-					>
-						<h3 className="vertical-timeline-element-title mimi-pink">Frontend Developer</h3>
-						<h4 className="vertical-timeline-element-subtitle">New Look</h4>
-						<p>JavaScript, CSS3, HTML5, Oracle Maxymiser, InVision, A/B Testing.</p>
-					</VerticalTimelineElement>
-
-					<VerticalTimelineElement
-						className="vertical-timeline-element--work vertical-timeline-element--four"
-						date="Nov 2018 - May 2019"
-						contentStyle={{ background: '#B8C0FF', color: '#4E4A52', fontSize: '12px' }}
-						contentArrowStyle={{ borderRight: '14px solid #B8C0FF' }}
-						iconStyle={{ background: '#B8C0FF', color: '#4E4A52' }}
-						icon={<img src={PP} alt="Pod Point" loading="lazy" />}
-					>
-						<h3 className="vertical-timeline-element-title mimi-pink">Frontend Developer</h3>
-						<h4 className="vertical-timeline-element-subtitle">Pod Point</h4>
-						<p>JavaScript, CSS3, HTML5, React, NPM, React Native, Xcode, Node.js, AWS, Agile.</p>
-					</VerticalTimelineElement>
-
-					<VerticalTimelineElement
-						className="vertical-timeline-element--work vertical-timeline-element--five"
-						date="May 2012 - Oct 2015"
-						contentStyle={{ background: '#94B2F5', color: '#4E4A52', fontSize: '12px' }}
-						contentArrowStyle={{ borderRight: '14px solid #94B2F5' }}
-						iconStyle={{ background: '#94B2F5', color: '#4E4A52' }}
-						icon={<img src={CD} alt="Copia Digital" loading="lazy" />}
-					>
-						<h3 className="vertical-timeline-element-title mimi-pink">Web Designer/Developer</h3>
-						<h4 className="vertical-timeline-element-subtitle">Copia Digital</h4>
-						<p>HTML5, CSS3, PHP, CMS, SVN, Adobe Photoshop, D3, Balsamiq, phpMyAdmin.</p>
-					</VerticalTimelineElement>
+					{experienceData.map((exp, index) => (
+						<VerticalTimelineElement
+							key={index}
+							className={`vertical-timeline-element--work vertical-timeline-element--${index + 1}`}
+							contentStyle={{background: exp.bgColor, color: '#4E4A52', fontSize: '12px'}}
+							contentArrowStyle={{borderRight: `14px solid ${exp.arrowColor}`}}
+							iconStyle={{background: exp.bgColor, color: '#FFF'}}
+							icon={<img src={exp.icon} alt={exp.subtitle} loading="lazy" />}
+						>
+							<h3 className={exp.titleClasses}>{exp.title}</h3>
+							<h2 className="vertical-timeline-element-subtitle">{exp.subtitle}</h2>
+							<ul className="tech__list">
+								{exp.techList.map((tech, index) => (
+									<li key={index} className={`tech__list--item tech__list--${tech}`}>{tech}</li>
+								))}
+							</ul>
+						</VerticalTimelineElement>
+					))}
 				</VerticalTimeline>
 
 				<div className="col-12">
